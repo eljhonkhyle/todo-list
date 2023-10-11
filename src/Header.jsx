4895a2ad30577d04365c18ce409213d9f13788c5
Ej <eljhonbalderama@gmail.com>
2023-10-12 02:19:33 +0800

    fixed:

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='header'>
-        <h1 className='logo'>Task Pro X</h1>
+        <h1 className='logo'>Task X Pro</h1>
     </div>
   )
 }
