96726f8e0814e5280a3f54a5099471c7a60f0a96
Ej <eljhonbalderama@gmail.com>
2023-11-02 03:59:37 +0800

    not yet fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,30 +10,6 @@ const Header = () => {
     localStorage.setItem('userName', newName); // Store the name in localStorage
   };
 
-  const containerStyles = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    textAlign: 'center',
-    marginLeft: '2rem'
-  };
-
-  const inlineContainerStyles = {
-    display: 'flex',
-    alignItems: 'center',
-  };
-
-  const inputStyles = {
-    border: 'none',
-    borderBottom: 'none',
-    outline: 'none',
-    background: 'none',
-    color: '#8758ff', 
-    width: '200px',
-    padding: '1rem',
-    fontSize: '1.7rem', 
-  };
-
   // Use useEffect to update the userName state when the component mounts
   useEffect(() => {
     const storedName = localStorage.getItem('userName');
@@ -43,16 +19,15 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="header" style={containerStyles}>
-      <h1 className="logo">Task X Pro</h1>
-      <div style={inlineContainerStyles}>
-        <h2>Welcome, </h2>
-        <input
+    
+    <div className="header-container">
+      <div className='inline-container'>
+        <h2 className='welcome-text'>Welcome, </h2>
+        <input className='welcome-input'
           type="text"
-          placeholder="Name here"
+          placeholder="Name Here"
           value={userName}
           onChange={handleNameChange}
-          style={inputStyles}
         />
       </div>
     </div>
